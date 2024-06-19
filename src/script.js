3aa9d7b62dcc8063372d8bf4a1faad18073892aa
connorrewa <connorrewa13@hotmail.com>
2024-06-18 20:53:32 -0400

    finish simple drawing logic

--- src/script.js
+++ src/script.js
@@ -1,26 +1,50 @@
 let container = document.querySelector(".container");
 container.addEventListener("mouseover", (event) => {
     let target = event.target;
-    target.classList.toggle("white");
+    target.classList.add("white"); // can use toggle() to have on/off func
 });
 
 container.addEventListener("mouseout", (event) => {
     target = event.target;
-    target.classList.toggle("red");
+    //target.classList.toggle("red");
+});
+
+let button = document.querySelector("button");
+button.addEventListener("click", () => {
+    console.log("button clicked");
+    let size = prompt("What size grid would you like?")
+    destroyGrid();
+    generateGrid(size);
+
 });
 console.log(container)
 console.log("running...")
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.classList.toggle("row");
-    container.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        let div = document.createElement("div");
-        div.classList.toggle("box");
-        div.classList.toggle(`${i}${j}`);
 
-        div.textContent = i;
-        row.appendChild(div);
-        console.log(div)
+function generateGrid(size) {
+    if (size > 100) {
+        alert("tooooooo big");
+        return;
     }
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.classList.toggle("row");
+        container.appendChild(row);
+        for (let j = 0; j < size; j++) {
+            let div = document.createElement("div");
+            div.classList.toggle("box");
+            div.classList.toggle(`${i}${j}`);
+    
+            // div.textContent = i;
+            row.appendChild(div);
+            console.log(div)
+        }
+    }
+    return;
 }
+
+function destroyGrid() {
+    document.querySelector(".container").innerHTML = '';
+    return;
+}
+
+generateGrid(32);
